6af8114df277148fd59f963e1e817f5b8fcfc4c3
Bill Adams <wjladams@gmail.com>
2016-05-20 22:08:16 -0400

    Added inconsistency and sample to ComparingEigenToNewCalc

--- Jupyter/prioritycalcs/basics/eigen.py
+++ Jupyter/prioritycalcs/basics/eigen.py
@@ -30,3 +30,54 @@ def largest_eigen(mat, error = 1e-10, use_harker = False):
         diff = max(abs(nextv - vec))
         vec = nextv
     return(vec)
+
+def inconsistency(mat, error = 1e-10, use_harker = False):
+    if use_harker:
+        mat = harker_fix(mat)
+    size = mat.shape[0]
+    vec = np.ones([size])
+    diff = 1
+    while diff > error:
+        nextv = np.matmul(mat, vec)
+        nextv = nextv/sum(nextv)
+        diff = max(abs(nextv - vec))
+        vec = nextv
+    nextv = np.matmul(mat, vec)
+    eigenval = sum(nextv)
+    ci = (eigenval-size)/(size-1)
+    rval = ci/riindex(size)
+    return(rval)
+
+def riindex(n):
+    if n==1:
+        return(1)
+    elif n==2:
+        return(1)
+    elif n==3:
+        return(0.58)
+    elif n==4:
+        return(0.90)
+    elif n==5:
+        return(1.12)
+    elif n==6:
+        return(1.24)
+    elif n==7:
+        return(1.32)
+    elif n==8:
+        return(1.41)
+    elif n==9:
+        return(1.45)
+    elif n==10:
+        return(1.49)
+    elif n==11:
+        return(1.51)
+    elif n==12:
+        return(1.48)
+    elif n==13:
+        return(1.56)
+    elif n==14:
+        return(1.57)
+    elif n==15:
+        return(1.59)
+    else:
+        return(1.6)
